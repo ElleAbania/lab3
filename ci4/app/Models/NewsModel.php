b061aaab2a89d4f89957f33d53fcd39a7cf18bed
emabania <emabania@student.apc.edu.ph>
2023-03-03 14:44:54 +0800

    removed git diff comments

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -7,14 +7,8 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'emabania_news';
-<<<<<<< HEAD
-    protected $allowedFields = ['title', 'slug', 'body'];
-=======
-    
     protected $allowedFields = ['title', 'slug', 'body'];
 
->>>>>>> e88638bcd7e3e3f5a9386ac902529d78433c214a
-    public function getNews($slug = false)
     {
         if ($slug === false) {
             return $this->findAll();
