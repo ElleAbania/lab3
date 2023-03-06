b0df965a279502c472555be6e6ebbc137f0077c5
emabania <emabania@student.apc.edu.ph>
2023-03-06 10:25:33 +0800

    Create guest entry based on CI4 Create New Item Tutorial

--- ci4/public/asset/inc/header.php
+++ ci4/public/asset/inc/header.php
@@ -3,8 +3,8 @@
         <li href="#" class="nav__logo" style="font-size: larger;">Elizabeth</li>
         <div class="nav__menu mt-lg-3" id="nav-menu">
             <ul class="nav__list">
-                <li class="nav__item"><a href="index.php" class="nav__link active-link">Home</a></li>
-                <li class="nav__item"><a href="contact.php" class="nav__link active-link">Contact</a></li>
+                <li class="nav__item"><a href="index" class="nav__link active-link">Home</a></li>
+                <li class="nav__item"><a href="contact" class="nav__link active-link">Contact</a></li>
                 <li>
                     <i class="change-theme fa-solid fa-moon" id="theme-button"></i>
                 </li>
